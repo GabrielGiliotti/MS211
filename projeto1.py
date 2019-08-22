176b140746584b8ee512604cad7296bda7f77086
Gabriel <gabrielgiliotti@hotmail.com>
2019-08-22 18:16:01 -0300

    Inicio do Algoritmo p1

---
+++ projeto1.py
@@ -0,0 +1,23 @@
+print("\nProjeto 1 - MS211 - Professor Florindo\n\n" +
+	"Linguagem de Programacao Utilizada: Python3\n" +
+	"Motivacao: Desejamos realizar a aproximacao e/ou encontrar raizes de funcoes atraves de metodos numericos.\n")
+
+print("Como exemplo de funcao, vamos tomar a equacao de Butler-Volmer em processos eletroquimicos que relaciona a\n" +
+      "densidade de corrente com o potencial em um eletrodo e pode ser escrita como:\n\n")
+
+print("			f(x) =  e^(a*x) - e^((a-1)*x) - b 		\n")
+
+print("Digite os valores de a e b:\nValor de a: ", end="")
+a = input()
+a = float(a)
+print("Valor de b: ", end="")
+b = input()
+b = float(b)
+
+e = 2.71828182846
+e = float(e)
+
+
+print(e)
+print(a,b)
+
