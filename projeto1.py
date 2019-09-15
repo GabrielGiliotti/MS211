78a83acd26a458b5a8bb93bca81d8cd2b049d591
giliotti <gabrielgiliotti@hotmail.com>
2019-09-15 18:42:03 -0300

    Formatado o melhor possivel e bug corrigido --> Eh com vc Erick kk

--- projeto1.py
+++ projeto1.py
@@ -10,7 +10,7 @@ print("Como exemplo de funcao, vamos tomar a equacao de Butler-Volmer em process
 
 print("			f(x) =  e^(alpha*x) - e^((alpha-1)*x) - beta 		, sendo alpha = 0.2 e beta = 2.0 \n\n")
 
-print("###############################################################################################################################")
+print("###############################################################################################################################\n")
 #Item a) a partir daqui
 print("Item a) Encontrando um intervalo que contenha uma raiz de f(x)\n", end="")
 print("Inicialmente vamos tomar arbitrariamente a funcao no intervalo x1, x2 e analisar seu grafico visualmente")
@@ -48,15 +48,29 @@ plt.xlabel('Eixo x [x1,x2]')
 plt.ylabel('Eixo y [f(x1), f(x2)]')
 plt.show()
 
-print("###############################################################################################################################")
+print("###############################################################################################################################\n")
 # Item b) a partir daqui.
 print("Item b) Implementacao dos metodos numericos Bisseccao e Newton\n")
 print("A funcao f(x) e o intervalo ja temos do item anterior.")
 print("Insira entao a tolerancia desejada e um numero maximo de iteracoes que o algoritmo pode realizar:\n")
 
 tol = float(input("Valor da tolerancia: "))
-imaxi = int(input("Valor do numero maximo de iteracoes: "))
+imaxi = 0
 print()
+
+print("Vamos estimar tambem, o numero de iteracoes necessarias para aproximar a raiz pensando no proprio metodo da Bisseccao.")
+print("Sabemos que o intervalo em cada iteracao k tera metade da amplitude do intervalo de iteracao k-1.\n")
+print("Temos entao x2k - x1k/2 , isto eh, valor de x2 na iteracao k menos valor de x1 na iteracao k , tudo dividido por 2, eh dado por:")
+print("(x20 - x10)/2^k (x2 na iteracao 0 menos x1 na itercacao 0 , tudo dividido por 2 elevado a k)")
+print("Finalmente, tal equacao deve ser menor que a tolerancia desejada.")
+print("Aplicando a operacao logaritmo neperiano em ambos os lados da inequacao, conseguimos isolar k, que eh o numero de iteracoes.")
+print("Finalmente o numero de iteracoes estimado eh dado por:\n")
+print("k > (ln(x20 - x10) - ln(tolerancia))/ ln(2)\n")
+
+def numIterac(x1,x2,tol):	
+	k = (mt.log(mt.exp(x2-x1)) - mt.log(mt.exp(tol)))/mt.log(mt.exp(2))
+	k = int(k)+1
+	return k
 
 # defini uma funcao para que possa ser chamada posteriormente (item c))
 def bisseccao(z1,z2,tole,imax):		
@@ -80,14 +94,11 @@ def bisseccao(z1,z2,tole,imax):
 	else:
 		print("Nao ha raizes no intervalo ")	
 		#return None
-		
 
-
-
+imaxi = numIterac(x1,x2,tol)		
 result = bisseccao(x1,x2,tol,imaxi)
 print("Raiz encontrada = " + str(result))
-
-	
+print()	
 #Metodo de Newton : Erick
 
 
@@ -101,21 +112,20 @@ print("Raiz encontrada = " + str(result))
 
 
 
-print("###############################################################################################################################")
+print("###############################################################################################################################\n")
 print("Item c) Encontrar raiz da equacao dada para diferentes pontos/intervalos iniciais e tabelar: ")
 print("Os intervalos passados [n1,n2], as aproximacoes das raizes e o numero de iteracoes.\n")
 
 print("Obs: Para todas as entradas que serao tabeladas, vamos tomar o a tolerancia utilizada no item b)\n")
 
-print("Defina aqui quantas linhas voce deseja em sua tabela: ")
-l = int(input())
+l = int(input("Defina aqui quantas linhas voce deseja em sua tabela: "))
 dTab = []
 j = 0
 while(j < l):
-	print("Insira na ordem: x1 intervalo, x2 intervalo e numero de iteracoes: ")	
+	print("Insira na ordem: x1 valor do intervalo e x2 valor do intervalo: ")	
 	x11 = float(input("valor x1: "))
 	x22 = float(input("valor x2: "))
-	itera = int(input("valor do numero de iteracoes: "))
+	itera = numIterac(x11,x22,tol)
 	r = bisseccao(x11,x22,tol,itera)
 	li =[x11,x22,r,itera]
 	dTab.append(li)	
@@ -126,20 +136,15 @@ print()
 print(" #### Tabela da Bisseccao ####")
 print("| valor x1  | valor x2  | raiz encontrada para f(x) | Numero de iteracoes |")
 for i in range(0,l):
-	if( flag == 2 ):
-		print("|  {}  |  {}  |  {}  |  {}  |".format(dTab[i][0],dTab[i][1],None,dTab[i][3]))
-	elif( flag == 1 ):
-		print("|  {}  |  {}  |  {}  |  {}  |".format(dTab[i][0],dTab[i][1],dTab[i][2],dTab[i][3]))
-	
-#	print("|    {""}   |".format(dTab[i][0]), end="")
-#	print("   {""}    |".format(dTab[i][1]), end="")
-#	if( dTab[i][2] < 0 ):
-#		print("         {}           |".format(None), end="")
-#	else:
-#		print("         {0:.7f}           |".format(dTab[i][2]), end="")
-#
-#
-#	print("         {}        |".format(dTab[i][3]))
+	print("|    {}    |".format(dTab[i][0]), end="")
+	print("   {}    |".format(dTab[i][1]), end="")
+	if( dTab[i][2] == None ):
+		print("           None            |", end="")
+	else:
+		print("         {0:.7f}           |".format(dTab[i][2]), end="")
+
+
+	print("         {}        |".format(dTab[i][3]))
 
 #Metodo de Newton : Erick
 
