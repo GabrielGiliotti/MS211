0ac8ab0983b3612818f3188c3e132d8a919db3f4
Gabriel <gabrielgiliotti@hotmail.com>
2019-11-24 18:43:18 -0200

    Algoritmo funciona com 1/6 na formulacao

--- projeto2.py
+++ projeto2.py
@@ -9,7 +9,7 @@ print("Como exemplo de funcao, vamos tomar a equacao de Pierre-Francois Verhulst
 
 print("			y(t) =  r * y(t) * (1 - (y(t)/K) )  , sendo r a taxa de reproducao e K a constante de capacidade do meio\n\n")
 
-print("###############################################################################################################################\n")
+print("##############################################################################################################################################################################\n")
 #Item a) a partir daqui
 print("Vamos utilizar a solucao analitica do problema para fazer as comparacoes com a aplicacao dos metodos numericos, que eh dada por:\n\n")
 
@@ -21,11 +21,11 @@ print("Utilizando r = 0.5, K = 10, h = 0.05 e y0 = 1" )
 print("Repare que, se o intervalo eh entre 0 e 4, queremos determinar y(4). Para a solucao analitica temos y(4) = 4.508530604")
 
 # ini se refere ao inicio do intervalo e fim se refere ao final do intervalo
-def metodoEuler(ini,fim,r,K,h,y0):
+def metodoEuler(ini,fim,r,K,h,y0): #para a equacao logistica dada
     t = ini   # t eh o valor de tempo onde vamos comecar no intervalo. y0 dado deve ser condizente com o valor de y(t0)  
     y_de_t = y0   # y_de_t eh o valor que y(t0) recebe, isso e, o valor y0 dado em nosso PVI --> y(t0) = y0
     it = 1    
-    while(t <= fim): # enquanto nao chegarmos em y(t = fim)     
+    while(t < fim): # enquanto nao chegarmos em y(t = fim)     
         y_n_mais_um = y_de_t + (h * (r * y_de_t * (1.0 - (y_de_t/K)))) # calculando y1, y2, ... , yn iteracoes     
         print("Valor na iteracao " + str(it) + " = " + str(y_n_mais_um) + " e Valor de t = " + str(t))
         it = it + 1 # incrementa para contagem de iteracoes
@@ -41,7 +41,7 @@ result = metodoEuler(0,4,0.5,10.0,0.05,1.0)
 print("\nComparando o resultado dado pelo algoritmo: " + str(result) + " com o resultado dado analiticamente y(4) = 4.508530604")
 print("Podemos verificar que o metodo de Euler se aproximou com precisao de apenas uma casa decimal em y(4)\n")
 
-print("###############################################################################################################################\n")
+print("############################################################################################################################################################################\n")
 
 print("Item b) Utilizando outros valores para os parametros inicialmente fornecidos, podemos verificar que:\n")
 print("Na alteracao do parametro h, quanto MAIOR seu valor, menor sera a precisao da equacao logistica e quanto MENOR seu valor, maior sera")
@@ -55,6 +55,7 @@ print("a precisao da equacao logistica.\n")
 #print("\nh = 0.001")
 #result = metodoEuler(0,4,0.5,10.0,0.001,1.0)
 
+
 print("Ja para o parametro r, temos que se a taxa de reproducao for alta (r grande), a equacao logistica aponta um aumento brusco na")
 print("populacao e logo em seguida um numero baixo, indicando que a populacao 'colapsou' devido aos poucos recursos (K).\n")
 
@@ -66,18 +67,53 @@ print("desenvolvimento da populacao, chegando ate mesmo em um ponto onde a popul
 print("Caso contrario, o meio limita o desenvolvimento de tal forma que nao importa a variacao do tempo, sempre havera um maximo que o")
 print("ambiente suporta.\n")
 
-#result = metodoEuler(0,4,0.5,1000,0.05,1.0)
+#result = metodoEuler(0,4,0.5,10000000,0.05,1.0)
 #result = metodoEuler(0,4,0.5,0.75,0.05,1.0)
 
-print("###############################################################################################################################\n")
+print("############################################################################################################################################################################\n")
 
-print("item c)")
-
-
-
+print("item c) Para o intervalo [0,4], com os dados do item a) temos uma aproximacao com certeza de 5 casas decimais, enquanto Euler obteve apenas uma casa decimal.")
+print("Ja para o intervalo [0,10], temos uma aproximacao com ate 8 casas decimais, o que diz que o metodo eh bastante preciso em comparacao com Euler.\n")
 
 
+def RungeKuttaOrdem4(ini, fim , r, K, h, y0): #para a equacao logistica dada
+	tk = ini
+	yk = y0
+	it = 1
+	while( tk < fim ):
+		# k1 recebe os parametros tk e yk iniciais, ou seja, t0 = 0 e y(t0) = y0 = 1 
+		
+		k1 = h*(r * yk *(1 - (yk/K)))   
+        # tkparak2 e ykparak2 sao os parametros que devemos passar para k2, que utilizam tk inicial, h, yk inicial e k1
+		tkparak2 = tk + (h/2.0)
+		ykparak2 = yk + (k1/2.0)
+		k2 = h*(r * ykparak2 * (1 - (ykparak2/K)))
+		
+        # tkparak3 e ykparak3 sao os parametros que devemos passar para k3, que utilizam tk inicial, h, yk inicial e k2
+		tkparak3 = tk + (h/2.0)
+		ykparak3 = yk + (k2/2.0)
+		k3 = h*(r * ykparak3 * (1 - (ykparak3/K)))
+		
+        # tkparak4 e ykparak4 sao os parametros que devemos passar para k4, que utilizam tk inicial, h, yk inicial e k3
+		tkparak4 = tk + h
+		ykparak4 = yk + k3
+		k4 = h*(r * ykparak4 * (1 - (ykparak4/K)))
+		 	
+		yk_mais_um = yk + ((1/6.0)*(k1+(2*k2)+(2*k3)+k4))
+		tk = tk + h
+		yk = yk_mais_um
+	
+		print("Valor na iteracao " + str(it) + " = " + str(yk_mais_um) + " e Valor de t = " + str(tk))
+		it = it + 1
+ 		
+		
+# Teste com dados iniciais
+#RungeKuttaOrdem4(0,4,0.5,10.0,0.05,1)
 
+# Teste com intervalo [0,10]
+#RungeKuttaOrdem4(0,10,0.5,10.0,0.05,1)
 
-
-
+print("#################################################################################################################################################################\n")
+ 
+print("item d) Analisando os dados (descomente as chamadas do algoritmo no script), temos que Runge Kutta, pela sua precisao, eh mais condizente com a solucao analitica que o metodo de Euler.")
+print("Relatorio ira melhor apresentar as explicacoes.\n") 
